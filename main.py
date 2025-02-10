2434267ac228c2af4315b1fe84054909a10ad545
gugasabry <guga_sabry@hotmail.com>
2025-02-09 21:00:06 -0300

    Commit 1.0

--- main.py
+++ main.py
@@ -2,29 +2,33 @@
 import cv2
 from PIL import Image
 import os
+import requests
 import io
 import gtts
 from playsound import playsound
 from pydub import AudioSegment
 import google.generativeai as genai
+'''from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from openai import OpenAI
+from langchain.schema import Document
+from langchain_community.callbacks.openai_info import OpenAICallbackHandler'''
 
 # Configurações da API do Gemini
 GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')  # Chave da API carregada do ambiente
 genai.configure(api_key="AIzaSyCix3QUmIhQQ-nY2NvuSf5-MxkDMqP_D14")
 
+#os.environ["OPENAI_API_KEY"] = "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"
+
 texto_prompt = "Extraia o texto contido nesta imagem! Não precisa dizer nada além disso, apenas enviar o texto sem nenhuma mensagem a mais!"
+
+documentos = []
 
 
 def extrai_texto_da_imagem(image_path):
-    """
-    Extrai texto de uma imagem usando a API Gemini.
-
-    Args:
-        image_path: Caminho para o arquivo de imagem.
-
-    Returns:
-        O texto extraído da imagem, ou None se ocorrer um erro.
-    """
     try:
         # Carregue a imagem
         img = Image.open(image_path)
@@ -46,6 +50,8 @@ def extrai_texto_da_imagem(image_path):
 
         # Obtenha o texto da resposta
         texto = requisicao.text
+
+        documentos.append(texto)
 
         return texto.strip()
 
@@ -94,6 +100,21 @@ def falar_texto(texto):
     os.remove("temp1.mp3")
 
 
+def responder_pergunta(pergunta, texto):
+    model = genai.GenerativeModel("gemini-pro")
+    prompt = f"Pergunta: {pergunta}\nTexto: {texto}"
+    resposta = model.generate_content([prompt])
+    return resposta.text
+
+
+def fazer_perguntas():
+    while True:
+        pergunta = input("Digite a pergunta: ")
+        conteudo = "".join(documentos)
+        texto = responder_pergunta(pergunta, conteudo)
+        print(texto.strip())
+
+
 def main():
     # Captura ou carrega a imagem
     caminho_imagem = ""
@@ -110,21 +131,25 @@ def main():
 
     caminho_imagem = "leitura.jpg"
     imagem = carregar_imagem(caminho_imagem)
+
     # Converte a imagem para o formato esperado pela API
     imagem.save('temp.jpg')
     with open('temp.jpg', 'rb') as f:
         imagem_bytes = f.read()
 
     # Mensagem de espera
-    falar_mensagem_inicial()
+    #falar_mensagem_inicial()
 
     # Envia a imagem para a API do Gemini
     texto = extrai_texto_da_imagem(caminho_imagem)
-    print(f"[{len(texto)}] {texto}")
+    #print(f"[{len(texto)}] {texto}")
     os.remove("temp.jpg")
 
     # Fala o texto extraído
-    falar_texto(texto)
+    #falar_texto(texto)
+
+    # Perguntas sobre o texto
+    fazer_perguntas()
 
 if __name__ == "__main__":
     main()
